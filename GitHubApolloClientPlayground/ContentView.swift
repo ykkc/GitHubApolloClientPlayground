02ce10258b0645edbac60e71a832f4aa03cee773
ykkc <ylane.admirable@gmail.com>
2020-05-13 04:48:18 +0900

    :+1: add info components

--- GitHubApolloClientPlayground/ContentView.swift
+++ GitHubApolloClientPlayground/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("user name")
+            Text("email")
+            Image(uiImage: UIImage(systemName: "icloud.and.arrow.down")!)
+        }
     }
 }
 
