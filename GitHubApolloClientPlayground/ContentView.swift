352b00f8cf0a61e26a090583b3b8abfbeb1e4a00
ykkc <ylane.admirable@gmail.com>
2020-05-13 02:47:36 +0900

    :sparkles: init project

---
+++ GitHubApolloClientPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubApolloClientPlayground
+//
+//  Created by ykkc on 2020/05/13.
+//  Copyright © 2020 ykkc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
